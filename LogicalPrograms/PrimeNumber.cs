b018acfce705de6be07a47a03125569a896326d0
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-18 18:32:25 +0530

    [Sachin] Adding Prime number

---
+++ LogicalPrograms/PrimeNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class PrimeNumber
+    {
+        public static void FindPrimeNumber(int number)
+        {
+            int count = 0;
+            for(int i = 1; i<=number; i++)
+            {
+                if (number % i == 0)
+                {
+                    count++;
+                }
+            }
+            if(count == 2)
+            {
+                Console.WriteLine("Number is Prime Number");
+            }
+            else
+            {
+                Console.WriteLine("Number is not a prime");
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -5,6 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To logical program");
+            Console.WriteLine("Please select the program given below");
+            Console.WriteLine("\n1:PrimeNumber");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Enter the number");
+                    int input = Convert.ToInt32(Console.ReadLine());
+                    PrimeNumber.FindPrimeNumber(input);
+                    break;
+                default:
+                    Console.WriteLine("Please select the given below program");
+                    break;
+            }
+
         }
     }
 }
