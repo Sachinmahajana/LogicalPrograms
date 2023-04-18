d98e35bf4ffebcf0b1caf4498f6acc892fb2f36a
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-18 21:21:02 +0530

    [Sachin] Adding ReverseNumber Program

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome To logical program");
             Console.WriteLine("Please select the program given below");
-            Console.WriteLine("\n1:PrimeNumber");
+            Console.WriteLine("\n1:PrimeNumber\n2:ReverseNumber");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -15,6 +15,11 @@
                     Console.WriteLine("Enter the number");
                     int input = Convert.ToInt32(Console.ReadLine());
                     PrimeNumber.FindPrimeNumber(input);
+                    break;
+                case 2:
+                    Console.WriteLine("Enter the number");
+                    int input1 = Convert.ToInt32(Console.ReadLine());
+                    ReverseNumber.FindReverseNumber(input1);
                     break;
                 default:
                     Console.WriteLine("Please select the given below program");
---
+++ LogicalPrograms/ReverseNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class ReverseNumber
+    {
+        public static void FindReverseNumber(int number)
+        {
+            
+            int reverse = 0;
+            while (number != 0)
+            {
+                int rem = number % 10;
+                reverse = (reverse * 10) + rem;
+                number = number / 10;
+            }
+            Console.WriteLine("After reverse the number is: " + reverse);    
+        }
+    }
+}
